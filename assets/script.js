431730cfe5da1f115a420f31933e466f54af41b2
English Camp <cecsungkim@gmail.com>
2025-06-19 19:47:58 -0700

    Delete assets/script.js

--- assets/script.js
+++
@@ -1,7 +0,0 @@
-document.querySelectorAll('.read-more').forEach(btn => {
-  btn.addEventListener('click', () => {
-    const extra = btn.nextElementSibling;
-    extra.classList.toggle('expanded');
-    btn.textContent = extra.classList.contains('expanded') ? 'Show Less' : 'Read More';
-  });
-});
