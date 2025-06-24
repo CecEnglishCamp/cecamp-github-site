244c583028065c114215be1b923cb31d7beb5299
English Camp <cecsungkim@gmail.com>
2025-06-23 23:27:26 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
-// 내비게이션 클릭→부드럽게 스크롤
-document.querySelectorAll('nav a').forEach(a => {
-  a.addEventListener('click', e => {
-    e.preventDefault();
-    document.querySelector(a.getAttribute('href')).scrollIntoView({ behavior: 'smooth' });
-  });
-});
+
