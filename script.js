01d248a89aff567b1eab4e2637d5a3a50a134635
English Camp <cecsungkim@gmail.com>
2025-06-24 22:49:16 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-```javascript
 // script.js
 
-// "Read More" 버튼 클릭 시 알림창 (기본 시연용)
+// For 'Read More' button expansion placeholder
+
 document.querySelectorAll(".read-more").forEach(button => {
   button.addEventListener("click", e => {
     e.preventDefault();
     alert("Read More section will expand here.");
   });
 });
-```
