a3fcdf6b29ab3870cc27a4ee967cd3196c7d4247
English Camp <cecsungkim@gmail.com>
2025-06-25 17:47:34 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,1 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const toggleButtons = document.querySelectorAll(".toggle-button");
 
-  toggleButtons.forEach(button => {
-    button.addEventListener("click", () => {
-      const content = button.previousElementSibling;
-      const isVisible = content.style.display === "block";
-
-      // 토글 동작
-      content.style.display = isVisible ? "none" : "block";
-      button.textContent = isVisible ? "Read more" : "Show less";
-    });
-  });
-});
